1cde264a46a326b1356ecb2a242ccdece3125f8a
mohovic0 <108498992+mohovic0@users.noreply.github.com>
2023-01-26 17:36:09 +0100

    Updates

---
+++ precache-manifest.06b2da36c77cdcd341aab543cf80e130.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7592621c5a0cfd168a4",
+    "url": "/static/css/main.4ed09d4c.chunk.css"
+  },
+  {
+    "revision": "e7592621c5a0cfd168a4",
+    "url": "/static/js/main.e7592621.chunk.js"
+  },
+  {
+    "revision": "cad2afd92761040d5c0c",
+    "url": "/static/js/1.cad2afd9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d7cdfef3595daa3189933f74e04398d",
+    "url": "/static/media/sigma.8d7cdfef.png"
+  },
+  {
+    "revision": "91d3bd0a6772cc2d172943047f744c9f",
+    "url": "/static/media/envelope-fill.91d3bd0a.svg"
+  },
+  {
+    "revision": "2cb3f0705ed05bee66797410fa6a7b48",
+    "url": "/static/media/facebook.2cb3f070.svg"
+  },
+  {
+    "revision": "c52aca6f51344dcffd3c3dcf3ce709bb",
+    "url": "/static/media/youtube.c52aca6f.svg"
+  },
+  {
+    "revision": "ceb976fe3c62bf64704a5f48c6460c2f",
+    "url": "/index.html"
+  }
+];
